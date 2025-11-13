43ad9b8a17e8c3477aed9fdabb89a90e2396fbb3
Cristiano Groberio <djcristiano.sgp@hotmail.com>
2025-11-13 13:21:09 -0300

    Ajustenovo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import React from 'react';
 import StartScreen from './componentes/StartScreen';
 import GameScreen from './componentes/GameScreen';
