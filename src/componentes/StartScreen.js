d09a38be5949da07bd6cdab88e7a136c8e456699
Cristiano Grobério <34129366+djcristianosgp@users.noreply.github.com>
2025-11-13 10:56:57 -0300

    Add name attribute to input field in StartScreen

--- src/componentes/StartScreen.js
+++ src/componentes/StartScreen.js
@@ -19,6 +19,7 @@ function StartScreen({ objectCount, setObjectCount, startGame, moveSpeed, setMov
       />
       <input
         type="text"
+        name="name"
         value={playerName}
         onChange={(e) => setPlayerName(e.target.value)}
         placeholder="Digite seu nome"
